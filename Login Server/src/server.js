1aad56810ff3df6280542deece9c786ed0821883
soplay <tkdgns1341@gmail.com>
2022-03-30 23:04:21 +0900

    [ Login ] cors middleware add

--- Login Server/src/server.js
+++ Login Server/src/server.js
@@ -5,7 +5,9 @@ const cookieParser = require("cookie-parser");
 const mongoose = require("mongoose");
 const { userRouter } = require("./routes/userRoute");
 const MONGO_URI = "mongodb://root:root@localhost:3333/LoginServer?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false";
+const cors = require("cors");
 
+app.use(cors());
 const server = async () => {
   try {
     let mongooseConnection = await mongoose.connect(MONGO_URI);
