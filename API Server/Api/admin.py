2e6ad7a36a37b500d9e480398546c141a5b6cd47
hod0ri <hodori991004@gmail.com>
2022-04-03 13:38:36 +0900

    [ API ] Set Admin

--- API Server/Api/admin.py
+++ API Server/Api/admin.py
@@ -1,3 +1,8 @@
+from re import M
 from django.contrib import admin
+from .models import Member, UpdatePoint, HireDriver, HireCustomer
 
-# Register your models here.
+admin.site.register(Member)
+admin.site.register(UpdatePoint)
+admin.site.register(HireDriver)
+admin.site.register(HireCustomer)
--- API Server/Api/views.py
+++ API Server/Api/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from .models import Member, UpdatePoint, HireDriver, HireCustomer
+from rest_framework.views import APIView
+from .serializers import MemberSerializer, UpdatePointSerializer, HireDriverSerializer, HireCustomerSerializer
 
 # Create your views here.
+class UserSequenceView(APIView):
+    pass
