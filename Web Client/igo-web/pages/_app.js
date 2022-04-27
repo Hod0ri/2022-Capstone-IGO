2834a17ce29e6410737e6c172b3732fd24cfea00
soplay <tkdgns1341@gmail.com>
2022-04-27 16:05:28 +0900

    [ Web ] reset-css add, folder structuring, update README.md

--- Web Client/igo-web/pages/_app.js
+++ Web Client/igo-web/pages/_app.js
@@ -1,3 +1,4 @@
+import "reset-css";
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
--- Web Client/igo-web/pages/index.js
+++ Web Client/igo-web/pages/index.js
@@ -1,6 +1,24 @@
 import Head from "next/head";
 import Image from "next/image";
+import styled from "styled-components";
+import Header from "../core/components/Header";
+
+const BodyContainer = styled.div`
+  width: 33%;
+  max-width: 500px;
+  height: 100vh;
+  margin: 0 auto;
+  border: 1px solid red;
+`;
 
 export default function Home() {
-  return <div>start</div>;
+  return (
+    <>
+      <BodyContainer>
+        <Header />
+        <div>내용 뜰 곳 ? </div>
+        <div>nav위치</div>
+      </BodyContainer>
+    </>
+  );
 }
