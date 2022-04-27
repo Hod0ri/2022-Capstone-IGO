6ce76cbdfadd27b920641be0551cc56fcd69bd03
hod0ri <hodori991004@gmail.com>
2022-04-27 15:31:01 +0900

    [ API ] Change Database and re-Modeling
    using Code with me (협업)

--- API Server/Api/views.py
+++ API Server/Api/views.py
@@ -7,14 +7,18 @@ from rest_framework.parsers import JSONParser
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from .models import Member, UpdatePoint, HireDriver, HireCustomer
-from .serializers import MemberSerializer, UpdateSerializer, HireDriverSerializer, HireCustomerSerializer
-from API.UserMethod.validators import CheckValidAccount
+from .models import Member
+from .serializers import MemberSerializer
+from .UserMethod.validators import CheckValidAccount
 
 
-# Create your views here.
+# 유저 엔드 포인트
 class UserView(APIView):
     def post(self, request):
+        """POST 회원가입(추가) 포인트
+            @description
+            회원 추가를 위한 Login Server - API Server 간 Post 통신
+        """
         tempdata = JSONParser().parse(request)
         tempdata['user_Driver'] = 1 if tempdata.get('user_Driver') else 0
         member_serializer = MemberSerializer(data=tempdata)
@@ -31,6 +35,10 @@ class UserView(APIView):
         return JsonResponse(response_json)
 
     def get(self, request):
+        """GET 로그인 포인트
+            @description
+            로그인을 위한 Login Server - API Server 간 GET 통신
+        """
         tempdata = JSONParser().parse(request)
         temp_id = tempdata.get('user_Id')
         try:
@@ -43,6 +51,10 @@ class UserView(APIView):
         return JsonResponse(response_json)
 
     def put(self, request):
+        """PUT 정보 수정 포인트
+            @description
+            회원 정보 수정을 위한 API Server - Client Server 간 PUT 통신
+        """
         tempdata = JSONParser().parse(request)
         tempdata['user_Driver'] = 1 if tempdata.get('user_Driver') else 0
         temp_id = tempdata.get('user_Id')
@@ -54,6 +66,10 @@ class UserView(APIView):
         return Response(member_update_serializer.data, status=status.HTTP_200_OK)
 
     def delete(self, request):
+        """DELETE 탈퇴 포인트
+            @description
+            회원 탈퇴를 위한 Login Server - API Server 간 DELETE 통신
+        """
         tempdata = JSONParser().parse(request)
         temp_id = tempdata.get('user_Id')
         userdata = Member.objects.get(user_Id=temp_id)
--- API Server/igoAPI/urls.py
+++ API Server/igoAPI/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from ..API.views import UserView
+from Api.views import UserView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
