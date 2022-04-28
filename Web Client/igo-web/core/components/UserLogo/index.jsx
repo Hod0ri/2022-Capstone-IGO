21af94180ccef7f529b3ca7b25e7ea9e41d6a148
soplay <tkdgns1341@gmail.com>
2022-04-28 22:32:39 +0900

    [ Web ] add UserLogo component ( + base icons )

---
+++ Web Client/igo-web/core/components/UserLogo/index.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import Mail from "./icon/male.png";
+import Femail from "./icon/femail.png";
+import Image from "next/image";
+import styled from "styled-components";
+
+const UserLogoContainer = styled.div`
+  width: 80px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  p {
+    margin-top: 10px;
+  }
+  font-size: ${(props) => props.theme.fontSize.md};
+`;
+
+const UserLogo = ({ icon = "mail", nickname = "닉네임x" }) => {
+  return (
+    <UserLogoContainer>
+      <Image src={icon == "mail" ? Mail : Femail} width={80} height={80} />
+      <p>{nickname}</p>
+    </UserLogoContainer>
+  );
+};
+
+export default UserLogo;
