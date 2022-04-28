53d68d52e799685eb92edb482ec3c549fe8da0d0
soplay <tkdgns1341@gmail.com>
2022-04-29 00:30:46 +0900

    [ Web ] add InputBox common component

---
+++ Web Client/igo-web/core/components/Common/InputBox.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import styled from "styled-components";
+
+const InputStyle = styled.input`
+  width: 100%;
+  height: 50px;
+  padding: 0px 10px;
+  font-size: ${(props) => props.theme.fontSize.sm};
+  border: 1px solid ${(props) => props.theme.color.lightgray};
+  border-radius: 12.5px;
+
+  &:focus {
+    outline: none;
+    border: 1px solid ${(props) => props.theme.color.black};
+  }
+`;
+
+const InputBox = ({ placeholder = "", setValue = () => {} }) => {
+  return (
+    <InputStyle
+      placeholder={placeholder}
+      onChange={(e) => setValue(e.target.value)}
+    />
+  );
+};
+
+export default React.memo(InputBox);
