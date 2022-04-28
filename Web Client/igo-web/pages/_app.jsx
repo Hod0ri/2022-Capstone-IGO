1f90e42ef4d8e5472d716c4ee016507bd1061f26
soplay <tkdgns1341@gmail.com>
2022-04-28 15:29:54 +0900

    [ Web ] Styled-components themeProvider apply

--- Web Client/igo-web/pages/_app.jsx
+++ Web Client/igo-web/pages/_app.jsx
@@ -1,5 +1,5 @@
 import { RecoilRoot } from "recoil";
-import { createGlobalStyle } from "styled-components";
+import { createGlobalStyle, ThemeProvider } from "styled-components";
 import reset from "styled-reset";
 
 const GlobalStyles = createGlobalStyle`
@@ -10,14 +10,36 @@ const GlobalStyles = createGlobalStyle`
     outline: none;
   }
 `;
-
+const theme = {
+  color: {
+    blue: "#01A6DE",
+    orange: "#F6961D",
+    green: "#8EC73C",
+    gray: "#707070",
+    lightgray: "#999999",
+    black: "#000000",
+    white: "#FFFFFF",
+    red: "#F93B3B",
+    yellow: "#FFE330",
+  },
+  fontSize: {
+    xl: "40px",
+    lg: "26px",
+    md: "20px",
+    sm: "16px",
+    xs: "12px",
+  },
+};
+//
 const MyApp = ({ Component, pageProps }) => {
   return (
     <>
-      <RecoilRoot>
-        <GlobalStyles />
-        <Component {...pageProps} />
-      </RecoilRoot>
+      <ThemeProvider theme={theme}>
+        <RecoilRoot>
+          <GlobalStyles />
+          <Component {...pageProps} />
+        </RecoilRoot>
+      </ThemeProvider>
     </>
   );
 };
--- Web Client/igo-web/pages/index.jsx
+++ Web Client/igo-web/pages/index.jsx
@@ -1,14 +1,13 @@
-import styled from "styled-components";
-import Header from "../core/components/header";
+import styled, { css } from "styled-components";
+import Header from "../core/components/Header";
 import Navbar from "../core/components/Navbar";
 
 const BodyContainer = styled.div`
-  box-sizing: border-box;
   padding: 5px;
   width: 500px;
   height: 100vh;
   margin: 0 auto;
-  border: 1px solid red;
+  border: 1px solid ${(props) => props.theme.color.red};
 
   @media screen and (max-width: 500px) {
     width: 100%;
