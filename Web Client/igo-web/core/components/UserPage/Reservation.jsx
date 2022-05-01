447de7ce91b21af767e864a29b74263c20078efc
UBAMTOL\yij03 <yij0325@gmail.com>
2022-05-01 22:45:26 +0900

    [ Web ] add UserPage components

---
+++ Web Client/igo-web/core/components/UserPage/Reservation.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import styled from 'styled-components';
+import Route from './Route';
+import ReservationButton from './ReservationButton';
+import Review from './Review';
+
+const ReservationContainer = styled.div`
+  border: 2px solid ${props=>props.theme.color.gray};
+  margin-top: 20px;
+  padding: 15px;
+  display: flex;
+  border-radius: 10px;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  width: 70%;
+  max-height: 250px;
+  height: auto;
+  background: ${props=>props.theme.color.white};
+`;
+
+const Reservation = () => {
+  return(
+    <ReservationContainer>
+      <Route />
+      <ReservationButton />
+      <Review />
+    </ReservationContainer>
+  );
+}
+
+export default Reservation;
