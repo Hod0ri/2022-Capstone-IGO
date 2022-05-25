0750f216f38bec86af22045a283ba5b3c6b7711a
soplay <tkdgns1341@gmail.com>
2022-05-25 19:31:24 +0900

    [ Web ] Fix map data ( query data )

--- Web Client/igo-web/core/components/KakaoMap/index.jsx
+++ Web Client/igo-web/core/components/KakaoMap/index.jsx
@@ -1,9 +1,9 @@
-import axios from "axios";
-import React, { useEffect, useState } from "react";
-import styled from "styled-components";
-import locationData from "../../etc/location/station.json";
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
+import locationData from '../../etc/location/station.json';
 
-const KakaoMap = ({ location = "안양" }) => {
+const KakaoMap = ({ location = '안양' }) => {
   const [loadKakao, setLoadKakao] = useState(false);
   const [fetchRouteErr, setFetchRouteErr] = useState(false);
   const [stateDatas, setStateDatas] = useState({});
@@ -11,12 +11,12 @@ const KakaoMap = ({ location = "안양" }) => {
 
   useEffect(() => {
     const { lat: latitude, lng: longitude } = locationData.filter(
-      (v) => v["name"] === location
+      (v) => v['name'] === location
     )[0];
 
     const target = [37.4035503, 126.930539];
     const loadKakaoMapScript = () => {
-      const $script = document.createElement("script");
+      const $script = document.createElement('script');
       $script.async = true;
       $script.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_MAP_REST_API_KEY}&autoload=false`;
 
@@ -25,7 +25,7 @@ const KakaoMap = ({ location = "안양" }) => {
           //api 필요 요소 추출
 
           let data = await axios
-            .get("https://apis-navi.kakaomobility.com/v1/directions", {
+            .get('https://apis-navi.kakaomobility.com/v1/directions', {
               headers: {
                 Authorization: `KakaoAK ${process.env.NEXT_PUBLIC_KAKAO_FIND_REST_API_KEY}`,
               },
@@ -41,7 +41,7 @@ const KakaoMap = ({ location = "안양" }) => {
           const { distance, duration } = data.sections[0];
           setStateDatas({ distance, duration });
           const bound = data.sections[0].bound;
-          const mapContainer = document.getElementById("map");
+          const mapContainer = document.getElementById('map');
           const mapOption = {
             center: new window.kakao.maps.LatLng(bound.min_y, bound.min_x), // 지도상 최소 좌표
             level: 3, // 지도의 확대 레벨
@@ -65,13 +65,13 @@ const KakaoMap = ({ location = "안양" }) => {
               );
             }
           });
-          console.log(linePath);
+
           var polyline = new kakao.maps.Polyline({
             path: linePath, // 선을 구성하는 좌표배열 입니다
             strokeWeight: 5, // 선의 두께 입니다
-            strokeColor: "#01A6DE", // 선의 색깔입니다
+            strokeColor: '#01A6DE', // 선의 색깔입니다
             strokeOpacity: 1, // 선의 불투명도 입니다 1에서 0 사이의 값이며 0에 가까울수록 투명합니다
-            strokeStyle: "solid", // 선의 스타일입니다
+            strokeStyle: 'solid', // 선의 스타일입니다
           });
 
           // 지도에 경로 표시
@@ -80,7 +80,7 @@ const KakaoMap = ({ location = "안양" }) => {
           //지도에 마커 표시
           const positions = [
             {
-              title: "대림대학교",
+              title: '대림대학교',
               latlng: new kakao.maps.LatLng(...target),
             },
             {
@@ -89,7 +89,7 @@ const KakaoMap = ({ location = "안양" }) => {
             },
           ];
           var imageSrc =
-            "https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png";
+            'https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png';
 
           for (var i = 0; i < positions.length; i++) {
             // 마커 이미지의 이미지 크기 입니다
@@ -109,7 +109,7 @@ const KakaoMap = ({ location = "안양" }) => {
         });
       };
 
-      $script.addEventListener("load", onLoadKakaoMap);
+      $script.addEventListener('load', onLoadKakaoMap);
       document.head.appendChild($script);
     };
 
@@ -123,10 +123,10 @@ const KakaoMap = ({ location = "안양" }) => {
       <KaKaoMapContainer>
         <StateContainer>
           <div id="kakaoMapDistance">
-            {Math.floor(stateDatas.distance / 1000) || "--"}Km
+            {Math.floor(stateDatas.distance / 1000) || '--'}Km
           </div>
           <div>
-            예상 소요시간 | {Math.round(stateDatas.duration / 60) || "--"}분
+            예상 소요시간 | {Math.round(stateDatas.duration / 60) || '--'}분
           </div>
         </StateContainer>
         <div id="map" />
@@ -145,12 +145,16 @@ const StateContainer = styled.div`
     background-color: ${(props) => props.theme.color.orange};
     color: ${(props) => props.theme.color.white};
     padding: 5px;
+    margin-right: 10px;
     border-radius: 5px;
   }
 `;
 
 const KaKaoMapContainer = styled.div`
   #map {
+    border-radius: 20px;
+    overflow: hidden;
+    box-shadow: 3px 3px 5px 0.1px ${(props) => props.theme.color.lightgray};
     aspect-ratio: 1 / 1;
   }
 `;
