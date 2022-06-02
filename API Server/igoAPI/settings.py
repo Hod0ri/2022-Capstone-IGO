1ba675b01bdb02d7a17518fa8b28b087fdee36c9
soplay <tkdgns1341@gmail.com>
2022-06-02 22:13:43 +0900

    [ Docker ] fix err ( route, loaction, ) and test Nginx

--- API Server/igoAPI/settings.py
+++ API Server/igoAPI/settings.py
@@ -118,7 +118,7 @@ TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
@@ -134,11 +134,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # CORS Exception Handling
 
 CORS_ORIGIN_ALLOW_ALL = True
-
-CORS_ORIGIN_WHITELIST = [
-    'http://127.0.0.1:8000',
-    'http://localhost:8000'
-]
 
 CORS_ALLOW_CREDENTIALS = True
 
