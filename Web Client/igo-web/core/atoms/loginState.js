a4eadbacc8e512bba077ec9bbb1eabb299e8cdaa
soplay <tkdgns1341@gmail.com>
2022-06-05 19:15:38 +0900

    [ Web ] Fix atoms in session

--- Web Client/igo-web/core/atoms/loginState.js
+++ Web Client/igo-web/core/atoms/loginState.js
@@ -1,6 +1,22 @@
 import { atom } from 'recoil';
 
+//recoil use session
+const sessionStorageEffect =
+  (key) =>
+  ({ setSelf, onSet }) => {
+    const savedValue = sessionStorage.getItem(key);
+    if (savedValue != null) {
+      setSelf(JSON.parse(savedValue));
+    }
+
+    onSet((newValue, _, isReset) => {
+      isReset
+        ? sessionStorage.removeItem(key)
+        : sessionStorage.setItem(key, JSON.stringify(newValue));
+    });
+  };
 export const atomUserName = atom({
-  default: '대림이',
+  default: '',
   key: 'atomUserName',
+  effects: [sessionStorageEffect('UserName')],
 });
