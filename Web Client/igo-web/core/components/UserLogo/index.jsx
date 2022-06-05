da589a4296740194defa9a5692ffce51e85c1525
soplay <tkdgns1341@gmail.com>
2022-06-05 18:33:13 +0900

    [ Web ] Fix fetch UserNick

--- Web Client/igo-web/core/components/UserLogo/index.jsx
+++ Web Client/igo-web/core/components/UserLogo/index.jsx
@@ -3,6 +3,8 @@ import Mail from './icon/male.png';
 import Femail from './icon/female.png';
 import Image from 'next/image';
 import styled from 'styled-components';
+import { useRecoilValue } from 'recoil';
+import { atomUserName } from '../../atoms/loginState';
 
 const UserLogoContainer = styled.div`
   width: 80px;
@@ -15,7 +17,8 @@ const UserLogoContainer = styled.div`
   font-size: ${(props) => props.theme.fontSize.md};
 `;
 
-const UserLogo = ({ icon = 'mail', nickname = '닉네임x' }) => {
+const UserLogo = ({ icon = 'mail' }) => {
+  const user_Nick = useRecoilValue(atomUserName);
   return (
     <UserLogoContainer>
       <Image
@@ -24,7 +27,7 @@ const UserLogo = ({ icon = 'mail', nickname = '닉네임x' }) => {
         height={80}
         alt="alt"
       />
-      <p>{nickname}</p>
+      <p>{user_Nick || '닉네임x'}</p>
     </UserLogoContainer>
   );
 };
