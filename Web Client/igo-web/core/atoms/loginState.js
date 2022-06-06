57924011303e436d57f38a6d3ae515999aad67a6
soplay <tkdgns1341@gmail.com>
2022-06-06 12:52:55 +0900

    [ Web ] Fix atom key err  - add atom key name + hexadecimal Number

--- Web Client/igo-web/core/atoms/loginState.js
+++ Web Client/igo-web/core/atoms/loginState.js
@@ -1,22 +1,7 @@
 import { atom } from 'recoil';
 
 //recoil use session
-const sessionStorageEffect =
-  (key) =>
-  ({ setSelf, onSet }) => {
-    const savedValue = sessionStorage.getItem(key);
-    if (savedValue != null) {
-      setSelf(JSON.parse(savedValue));
-    }
-
-    onSet((newValue, _, isReset) => {
-      isReset
-        ? sessionStorage.removeItem(key)
-        : sessionStorage.setItem(key, JSON.stringify(newValue));
-    });
-  };
-export const atomUserName = atom({
+export const atomUserNick = atom({
   default: '',
-  key: 'atomUserName',
-  effects: [sessionStorageEffect('UserName')],
+  key: `atomUserNick/${Math.random().toString(36)}`,
 });
