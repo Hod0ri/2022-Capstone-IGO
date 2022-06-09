9facbae3ae7c6806f19dd7b7472f408c12806e2a
soplay <tkdgns1341@gmail.com>
2022-06-09 15:50:40 +0900

    [ Web ] Fix useLoadingState add isLoading state

--- Web Client/igo-web/core/hooks/useLoginState.js
+++ Web Client/igo-web/core/hooks/useLoginState.js
@@ -7,16 +7,22 @@ export default function useLoginState() {
   const fetcher = (url) => axios.get(url).then((res) => res.data);
   const setUserNick = useSetRecoilState(atomUserNick);
   const setUserDriver = useSetRecoilState(atomUserDriver);
+  let isLoading = false;
   let loginState = false;
+
   const { data, error } = useSWR(
     'https://igo.soplay.dev/api/auth/user',
     fetcher
   );
+  if (!data) {
+    isLoading = true;
+  }
   if (!error && data) {
     loginState = true;
+    isLoading = false;
     setUserNick(data.user_Nick);
     setUserDriver(data.user_Driver);
   }
 
-  return { loginState };
+  return { loginState, isLoading };
 }
