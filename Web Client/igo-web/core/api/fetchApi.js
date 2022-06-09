b87b7db7156493818bd792b686d99922cf1a0bbd
soplay <tkdgns1341@gmail.com>
2022-06-09 15:22:53 +0900

    [ Web ] Add fetchApi logics + add issue, match, matchlog, search

--- Web Client/igo-web/core/api/fetchApi.js
+++ Web Client/igo-web/core/api/fetchApi.js
@@ -6,6 +6,25 @@ axios.defaults.withCredentials = true;
 export const fetchApi = {
   point: {
     post: async (data) => await axios.post(`${BASE_URL}/point/`, data),
-    get: async () => await axios.get(`https://igo.soplay.dev/api/point/`),
+    get: async () => await axios.get(`${BASE_URL}/api/point/`),
+  },
+  issue: {
+    post: async (data) => await axios.post(`${BASE_URL}/issue/`, data),
+    get: async () => await axios.get(`${BASE_URL}/issue/`),
+  },
+  match: {
+    post: async (data) => await axios.post(`${BASE_URL}/match/`, data),
+    get: async () => await axios.get(`${BASE_URL}/match/`),
+  },
+  matchlog: {
+    post: async (data) => await axios.post(`${BASE_URL}/matchlog/`, data),
+    get: async () => await axios.get(`${BASE_URL}/matchlog/`),
+    put: async (data) => await axios.put(`${BASE_URL}/matchlog/`, data),
+    delete: async () => await axios.delete(`${BASE_URL}/matchlog/`),
+  },
+  search: {
+    //현재 기능은 출발지만 구현되어 있어서 쿼리로 arrive만 보낸다
+    get: async (query) =>
+      await axios.get(`${BASE_URL}/search/?arrive=${query}`),
   },
 };
