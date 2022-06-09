cb6c1afd81f712a82c6f738f57ff36ce554e03a2
9u4a <99junsung@gmail.com>
2022-06-10 02:47:58 +0900

    [ API ] Implement SearchView default option

--- API Server/Api/Views/SearchView.py
+++ API Server/Api/Views/SearchView.py
@@ -9,13 +9,29 @@ from Api.models import MatchData
 class SearchView(APIView):
     def get(self, request):
         if CheckUserID(request):
-            arrive = str(request.GET.get('arrive'))
+            arrive = request.GET.get('arrive')
             if arrive:
                 SearchArrive = list(MatchData.objects.filter(mc_Arrive=arrive).values())
                 if SearchArrive:
                     response_json = {
                         'success': True,
                         'data': SearchArrive,
+                        'err': ''
+                    }
+                    return JsonResponse(response_json, status=status.HTTP_200_OK)
+                else:
+                    response_json = {
+                        'success': False,
+                        'err': 'MatchData Does Not Exist'
+                    }
+                    return JsonResponse(response_json, status=status.HTTP_404_NOT_FOUND)
+
+            else:
+                SearchAll = list(MatchData.objects.filter().values())
+                if SearchAll:
+                    response_json = {
+                        'success': True,
+                        'data': SearchAll,
                         'err': ''
                     }
                     return JsonResponse(response_json, status=status.HTTP_200_OK)
