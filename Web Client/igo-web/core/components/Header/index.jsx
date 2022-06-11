2796904539698528736710d7d01cc0d3ca83e0cf
soplay <tkdgns1341@gmail.com>
2022-06-11 20:19:44 +0900

    [ Web ] Add logout event

--- Web Client/igo-web/core/components/Header/index.jsx
+++ Web Client/igo-web/core/components/Header/index.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import styled from 'styled-components';
-import { GoChevronLeft } from 'react-icons/go';
-import { HiOutlineMenu } from 'react-icons/hi';
+import { HiLogout } from 'react-icons/hi';
+import useLoginState from '../../hooks/useLoginState';
+import { fetchAuth } from '../../api/fetchAuth';
+import { useRouter } from 'next/router';
 
 const HeaderStyles = styled.div`
   max-width: 500px;
   width: 100%;
   height: 80px;
   padding: 20px;
+  backdrop-filter: blur(5px);
   background-color: ${(props) => props.theme.color.blue};
   color: ${(props) => props.theme.color.white};
   display: flex;
@@ -38,19 +41,23 @@ const MenuIcon = styled.div`
   flex: 1;
   justify-content: right;
   align-items: center;
-  font-size: 50px;
+  font-size: 40px;
   cursor: pointer;
 `;
 
 const Header = () => {
+  const router = useRouter();
+  const onClickLogout = async () => {
+    await fetchAuth.logout();
+    router.push('/signin');
+  };
+  const { loginState } = useLoginState();
   return (
     <HeaderStyles>
-      <BackButton>
-        <GoChevronLeft />
-      </BackButton>
+      <BackButton>{/* <GoChevronLeft /> */}</BackButton>
       <Title>IGO</Title>
-      <MenuIcon>
-        <HiOutlineMenu />
+      <MenuIcon onClick={() => onClickLogout()}>
+        {loginState && <HiLogout />}
       </MenuIcon>
     </HeaderStyles>
   );
