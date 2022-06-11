a931bdbc63379cb9de9ce9ec1d2440dd1e2968b2
Mallang-Mallang <gudwns1458@naver.com>
2022-06-11 15:51:32 +0900

    [ Web ] Fix FindId, Pw Logic

--- Web Client/igo-web/pages/findid.jsx
+++ Web Client/igo-web/pages/findid.jsx
@@ -1,26 +1,8 @@
-import React, { useEffect, useState } from 'react';
-import { useRouter } from 'next/router';
-import useLoginState from '../core/hooks/useLoginState';
+import React from 'react';
 import Auth from '../core/components/Common/Auth';
 import FindId from '../core/components/FindId';
-import { fetchAuth } from '../core/api/fetchAuth';
 
 const findid = () => {
-  const [state, setState] = useState(false);
-
-  const router = useRouter();
-  useEffect(() => {
-    const logout = async () => {
-      await fetchAuth
-        .refresh()
-        .then(() => {
-          useLoginState();
-          router.push('/');
-        })
-        .catch((e) => {});
-    };
-    !state && logout();
-  }, [state]);
   return (
     <Auth>
       <FindId />
--- Web Client/igo-web/pages/findpw.jsx
+++ Web Client/igo-web/pages/findpw.jsx
@@ -1,26 +1,8 @@
-import React, { useEffect, useState } from 'react';
-import { useRouter } from 'next/router';
-import useLoginState from '../core/hooks/useLoginState';
+import React from 'react';
 import Auth from '../core/components/Common/Auth';
 import FindPw from '../core/components/FindPw';
-import { fetchAuth } from '../core/api/fetchAuth';
 
 const findpw = () => {
-  const [state, setState] = useState(false);
-
-  const router = useRouter();
-  useEffect(() => {
-    const logout = async () => {
-      await fetchAuth
-        .refresh()
-        .then(() => {
-          useLoginState();
-          router.push('/');
-        })
-        .catch((e) => {});
-    };
-    !state && logout();
-  }, [state]);
   return (
     <Auth>
       <FindPw />
