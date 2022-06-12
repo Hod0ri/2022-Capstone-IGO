fc43c5e727290a55dca658f3f4e45632608f89ff
soplay <tkdgns1341@gmail.com>
2022-06-12 23:07:34 +0900

    [ Web ] Add MetaData

--- Web_Client/pages/_document.jsx
+++ Web_Client/pages/_document.jsx
@@ -1,5 +1,9 @@
 import Document, { Head, Html, Main, NextScript } from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
+const meta = {
+  title: 'IGO',
+  description: '대림대학교 카풀 공유 플랫폼',
+};
 export default class MyDocument extends Document {
   static async getInitialProps(ctx) {
     const sheet = new ServerStyleSheet();
@@ -39,6 +43,18 @@ export default class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR&display=swap"
             rel="stylesheet"
           />
+          {/* MetaData */}
+          <title>{meta.title}</title>
+          <meta name="description" content={meta.description} />
+          <meta
+            name="viewport"
+            content="initial-scale=1.0, width=device-width"
+          />
+          <meta property="og:title" content={meta.title} />
+          <meta property="og:type" content="website" />
+          <meta property="og:url" content={meta.url} />
+          {/* <meta property="og:image" content={image} /> */}
+          <meta property="og:article:author" content={meta.title} />
         </Head>
         <body>
           <Main />
