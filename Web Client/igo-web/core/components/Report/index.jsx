15470df493c7faa0fad93fae6f846eb225a50943
soplay <tkdgns1341@gmail.com>
2022-06-12 14:05:51 +0900

    [ Web ] Fix report null data state

--- Web Client/igo-web/core/components/Report/index.jsx
+++ Web Client/igo-web/core/components/Report/index.jsx
@@ -18,40 +18,15 @@ const Report = () => {
           setFetchMatchlogData(res.data.data);
         })
         .catch((err) => {
-          alert('서버 통신 오류');
           console.log(err.response.data);
         });
     };
     !fetchMatchlogData && fetchMatch();
   });
 
-  // const data = [
-  //   {
-  //     mm_Driver: '운전자닉네임',
-  //     mm_Member: '탑승자닉네임',
-  //     mm_ArriveTime: '2022-05-30 10:10',
-  //     mm_Arrive: '독산',
-  //     mm_Goal: '',
-  //     mm_Price: 2000,
-  //     mm_Match: true,
-  //   },
-  // {
-  //   mm_Driver: 'user3',
-  //   mm_Member: 'user4',
-  //   mm_ArriveTime: '2022-05-30 10:10',
-  //   mm_Arrive: '대림',
-  //   mm_Goal: '',
-  //   mm_Price: 3000,
-  //   mm_Match: false,
-  // },
-  // ];
-  // ns_Target	String	✔️
-  // ns_Reason	String	✔️
-  // ns_Etc
-
   return (
     <ReportCardContainer>
-      {fetchMatchlogData &&
+      {fetchMatchlogData ? (
         fetchMatchlogData.map((value, index) => {
           const data = {
             mm_Driver: value[`${fetchDataType}_Driver`],
@@ -63,7 +38,10 @@ const Report = () => {
             mm_Match: value[`${fetchDataType}_Match`],
           };
           return <ReportCard {...data} key={`reportCard_${index}`} />;
-        })}
+        })
+      ) : (
+        <ReportNoData>IGO 이용 기록이 없습니다!!</ReportNoData>
+      )}
     </ReportCardContainer>
   );
 };
@@ -71,6 +49,11 @@ const ReportCardContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+`;
+const ReportNoData = styled.div`
+  font-size: ${(props) => props.theme.fontSize.lg};
+  color: ${(props) => props.theme.color.gray};
+  padding: 30px 0;
 `;
 
 export default Report;
